729d2367154d6d9ecbf5f4832b03fe10be822830
Darren921 <darren.chung@ontariotechu.net>
2024-09-18 15:01:10 -0400

    Grammar mistakes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,12 +20,12 @@ public class GameManager : MonoBehaviour
         float minutes = Mathf.FloorToInt(Timer / 60f);
         float seconds = Mathf.FloorToInt(Timer - minutes * 60);
         string displayTime = string.Format("{0:00}:{1:00}", minutes, seconds);
-        TimerDisplay.text = "Time till HUNT: " + displayTime;
+        TimerDisplay.text = "Time till the HUNT: " + displayTime;
         Timer -= Time.deltaTime;
         if (Timer <= 0)
         {
             Timer = 0;
-            TimerDisplay.text = "IT KNOW YOUR LOCATION!";
+            TimerDisplay.text = "IT KNOWS YOUR LOCATION!";
             monsterAI._IsHunting = true;
         }
     }
