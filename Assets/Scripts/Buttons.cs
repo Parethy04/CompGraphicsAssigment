dc71136332845f54630198098ea051d719bd8ce3
Alex-Hamblin <alexmachamblin@gmail.com>
2024-10-03 17:21:15 -0400

    added rocks and mini tutorial

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-   
+    [SerializeField] GameObject uitext;
+    [SerializeField] GameObject uibuttons;
     public void Restart()
     {
         SceneManager.LoadScene("SampleScene");
@@ -17,13 +18,20 @@ public class Buttons : MonoBehaviour
     }
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        StartCoroutine(LoadScreen());
     }
 
     public void ToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
-    
+    private IEnumerator LoadScreen()
+    {
+        uitext.SetActive(true);
+        uibuttons.SetActive(false);
+        yield return new WaitForSeconds(8);
+        
+        SceneManager.LoadScene("SampleScene");
+    }
     
 }
